b75838fb256cf7656c0fdec5491232574b8e1f06
chunsikman <sb97213@gmail.com>
2024-09-20 15:48:15 +0900

    update 수정

--- src/App.vue
+++ src/App.vue
@@ -13,37 +13,19 @@ const updateDownloaded = ref(false);
 onMounted(() => {
   router.push('/');
 
+  checkForUpdate();
+
   window.winAPI.updateAvailable(() => {
     updateAvailable.value = true;
+    showUpdateAlert();
   });
-
   window.winAPI.updateNotAvailable(() => {
     updateAvailable.value = false;
-  });
 
+  });
   window.winAPI.updateDownloaded(() => {
     updateDownloaded.value = true;
   });
-
-  if(updateAvailable.value) {
-    checkForUpdate();
-    Swal.fire({
-      showClass: {
-        popup: ''
-      },
-      hideClass: {
-        popup: ''
-      },
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      text: '새 업데이트가 있습니다.',
-      confirmButtonText: '업데이트 설치',
-      preConfirm: () => {
-        installUpdate();
-      }
-    });
-  }
-
 });
 
 function checkForUpdate() {
@@ -52,6 +34,24 @@ function checkForUpdate() {
 
 function installUpdate(){
   window.winAPI.quitAndInstall();
+}
+
+function showUpdateAlert() {
+  Swal.fire({
+    showClass: {
+      popup: ''
+    },
+    hideClass: {
+      popup: ''
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    text: '새 업데이트가 있습니다.',
+    confirmButtonText: '업데이트 설치',
+    preConfirm: () => {
+      installUpdate();
+    }
+  });
 }
 
 </script>
